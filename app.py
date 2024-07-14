10a006a9f15a9295398a4a20288c1540ba4464a6
Vasudev Gupta <122823705+vasudevgupta62218@users.noreply.github.com>
2024-07-14 13:18:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from multiapp import Multiapp
+from apps import Poisson_dist,Normal_dist
+
+app=Multiapp()
+app.add_app("Poisson Distribution",Poisson_dist.app)
+app.add_app("Normal Distribution",Normal_dist.app)
+
+app.run()
