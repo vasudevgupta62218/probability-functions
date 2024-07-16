bd3bebb41fb74fc4470c4bb144e8e505bfa522f9
Vasudev Gupta <vasudevgupta99710@gmail.com>
2024-07-17 02:00:47 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from multiapp import Multiapp
-from PDF import Poisson_dist,Normal_dist
+from PDF import Poisson_dist,Normal_dist,Binomial_dist
 
 app=Multiapp()
 app.add_app("Poisson Distribution",Poisson_dist.app)
 app.add_app("Normal Distribution",Normal_dist.app)
+app.add_app("Binomial Distribution",Binomial_dist.app
 
 app.run()
