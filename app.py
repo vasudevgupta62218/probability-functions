bcd15f94a738dd8beb4f5c188cfca620b8b026ca
Vasudev Gupta <vasudevgupta99710@gmail.com>
2024-07-17 02:03:11 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from PDF import Poisson_dist,Normal_dist,Binomial_dist
 app=Multiapp()
 app.add_app("Poisson Distribution",Poisson_dist.app)
 app.add_app("Normal Distribution",Normal_dist.app)
-app.add_app("Binomial Distribution",Binomial_dist.app
+app.add_app("Binomial Distribution",Binomial_dist.app)
 
 app.run()
