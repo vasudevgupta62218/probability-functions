781777eb7c00d8d74cac224d647ad2d757585881
Vasudev Gupta <vasudevgupta99710@gmail.com>
2024-08-02 08:25:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from multiapp import Multiapp
-from PDF import Poisson_dist,Normal_dist,Binomial_dist
+from PDF import Poisson_dist,Normal_dist,Binomial_dist,Exponential_dist
 
 app=Multiapp()
 app.add_app("Poisson Distribution",Poisson_dist.app)
 app.add_app("Normal Distribution",Normal_dist.app)
 app.add_app("Binomial Distribution",Binomial_dist.app)
+app.add_app("Exponential Distribution",Exponential_dist.app)
 
 app.run()
